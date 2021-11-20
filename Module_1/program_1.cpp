b22254f06424611398f2386aadcda082e38a3949
hgg <hgg@pop-os.localdomain>
2021-11-20 08:25:17 -0500

    initial commit

---
+++ Module_1/program_1.cpp
@@ -0,0 +1,17 @@
+/*
+	This is a simple C++ program
+
+	Call this program_1.cpp
+
+*/
+
+#include <iostream>
+using namespace std;
+
+// A C++ program begins at main(),
+int main ()
+{
+	cout << "C++ is power programming.\n";
+
+	return 0;
+}
