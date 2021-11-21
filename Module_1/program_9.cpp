b7a9a9ca0f20eaae27414e7ef06ec790cc0a8a50
hggumich <hggumich@gmail.com>
2021-11-21 09:29:44 -0500

    add program_9

---
+++ Module_1/program_9.cpp
@@ -0,0 +1,16 @@
+// use the abs () function
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+	
+	int result;
+
+	result = abs(-10);
+
+	cout << result << "\n";
+
+	return 0;
+}
