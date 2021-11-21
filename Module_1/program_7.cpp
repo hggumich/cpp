a909291b9c2545df2aa5cc2c3035414a2fd55806
hggumich <hggumich@gmail.com>
2021-11-21 08:50:30 -0500

    add program 7

---
+++ Module_1/program_7.cpp
@@ -0,0 +1,13 @@
+// A program that illustrates the for loop
+
+#include<iostream>
+using namespace std;
+
+int main () {
+	int count;
+
+	for(count=1; count <= 100; count=count+1)
+		cout << count << "\n";
+
+	return 0;
+}
