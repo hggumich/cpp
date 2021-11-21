b1b9be5f1e71b8393e988fc16de76c3ad21c4d60
hggumich <hggumich@gmail.com>
2021-11-21 08:41:58 -0500

    add program 6

---
+++ Module_1/program_6.cpp
@@ -0,0 +1,33 @@
+// Demonstrate the if
+
+#include<iostream>
+using namespace std;
+
+int main () {
+	int a, b, c;
+
+	a = 2;
+	b = 3;
+
+	if(a < b) cout << "a is less than b\n";
+
+	// this won't display anything
+	if(a == b) cout << "a is equal to b\n";
+
+	cout << "\n";
+
+	c = a - b; 
+
+	cout << "c contains -1\n";
+	if(c >= 0) cout << "c is non-negative\n";
+	if(c < 0) cout << "c is negative\n";
+
+	cout << "\n";
+
+	c = b - a;
+	cout << "c contains 1\n";
+	if(c >= 0) cout << "c is non-negative\n";
+	if(c < 0) cout << "c is negative\n";
+
+	return 0;
+}
